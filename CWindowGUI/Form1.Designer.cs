421a32844d2b2245b1bd6e66044ba5917bc91dfd
chukwai98909009 <72026037+chukwai98909009@users.noreply.github.com>
2020-09-28 09:39:56 -0400

    add code numero dos

---
+++ CWindowGUI/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CWindowGUI
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtbox
+            // 
+            this.txtbox.Location = new System.Drawing.Point(407, 185);
+            this.txtbox.Name = "txtbox";
+            this.txtbox.Size = new System.Drawing.Size(100, 20);
+            this.txtbox.TabIndex = 0;
+            this.txtbox.TextChanged += new System.EventHandler(this.txtbox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbox;
+    }
+}
+
